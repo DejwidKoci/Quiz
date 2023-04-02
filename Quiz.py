b8d3f52995022da78bb102609f944bf09051c95d
Dejwid <s205915@sggw.edu.pl>
2023-04-02 11:20:57 +0200

    title and question

--- Quiz.py
+++ Quiz.py
@@ -15,6 +15,28 @@ class Quiz:
         self.options = (data['options'])
         self.answer = (data['answer'])
 
+        self.q_number = 0
+        self.display_title()
+        self.display_question()
+
+        self.run()
+
+
+
+    def display_title(self):
+        self.title = Label(self.window, text = "LIVERPOOL FC QUIZ", width = 50, bg = "red",
+                      fg = "white", font = ("ariel", 20, "bold"))
+        self.title.place(x = 0, y = 2)
+
+    def display_question(self):
+        
+        self.dis_question = Label(self.window, text = self.question[self.q_number], width = 60,
+                                font=("ariel" ,16, "bold" ), anchor = 'w' )
+         
+        self.dis_question.place(x = 70, y = 100)
+
+
+
     def run(self):
         self.window.mainloop()
 
